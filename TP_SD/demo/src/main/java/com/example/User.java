d1352c0ce9acc038fb142dad308039a0cc048a68
Vasco Coelho <vascocoelho@MacBook-Air-de-Vasco.local>
2023-12-04 10:51:01 +0000

    commitUser
    Método User

--- TP_SD/demo/src/main/java/com/example/User.java
+++ TP_SD/demo/src/main/java/com/example/User.java
@@ -5,5 +5,51 @@ public class User {
     private int ID;
     private String name;
     private Profiles profile;
+    private boolean isActive;
+
+    public User(int ID, String name, Profiles profile, boolean isActive) {
+        this.ID = ID;
+        this.name = name;
+        this.profile = profile;
+        this.isActive = isActive;
+    }
+    
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Profiles getProfile() {
+        return profile;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfile(Profiles profile) {
+        this.profile = profile;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public void sendMessage(User receiver, String message){
+
+        Message msg = new Message(this, receiver, message);
+
+    }
   
 }
